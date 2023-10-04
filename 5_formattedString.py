bc0b325da8d77fa2385dca0c596cde2241a7c725
arafat-yasin-2413 <129652012+arafat-yasin-2413@users.noreply.github.com>
2023-10-04 08:55:35 +0600

    first commit

---
+++ 5_formattedString.py
@@ -0,0 +1,7 @@
+first ="Kamal"
+last = "Uddin"
+
+
+print(f'{first} [{last}] is a coder')
+# message = first + ' ['+ last+']' +' is a coder'
+# print(message)
---
+++ 6_string_methods.py
@@ -0,0 +1,15 @@
+course = "Python for beginners"
+length = len(course)
+# print(f'Length : {length}')
+print(f'Length is : ',len(course))
+print(f'Lowercase : ',course.lower())
+print(f'Uppercase : ',course.upper())
+print(course.find('for'))
+print(course.replace('beginners','Absolute Beginners'))
+print(course.replace('P','J'))
+
+print('********************************')
+print()
+
+print('Python' in course)
+
